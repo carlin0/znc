3ccb0952cb7368d15b08c50c21b981f7b341102c
Carlo Ponti <carlin0@email.it>
2019-02-02 00:09:21 +0100

    Update aka.py

--- aka.py
+++ aka.py
@@ -14,7 +14,7 @@
 #  You should have received a copy of the GNU General Public License along
 #  with this program; if not, write to the Free Software Foundation, Inc.,
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-### dipendenze python3-request sqlite3
+### dipendenze python3-requests sqlite3
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #  Authors: Evan (MuffinMedic), Aww (AwwCookies)                          #
 #  Contributors: See CHANGELOG for specific contributions by users        #
